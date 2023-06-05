e3533225b4625bbb232e77a0bf1fe4426b843e3a
Andres Vargas <a.m.vargas1996@gmail.com>
2023-06-05 19:07:28 -0300

    Traduce los labels de Auth de supabase. Pendiente los mensajes de errores (falta de soporte)

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import useAuthModal from "@/hooks/useAuthModal";
+import { useEffect } from "react";
 
 /* Componente de modal para autenticacion. Utiliza el componente Modal de base. */
 const AuthModal = () => {
@@ -14,6 +15,14 @@ const AuthModal = () => {
   const router = useRouter();
   const { session } = useSessionContext();
   const { onClose, isOpen } = useAuthModal(); // Importa las funciones de apertura y cierre de modal
+  
+  // Si hay una sesion abierta, refresca la pagina y cierra el modal
+  useEffect(() => {
+    if(session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
 
   const onChange = (open: boolean) => {
     if (!open) onClose();
@@ -27,6 +36,52 @@ const AuthModal = () => {
       onChange={onChange}
     >
       <Auth 
+          localization={{ // Para traducir los labels de la autenticacion. De momento no traduce los errores, pues no hay soporte para ello. ¿Tal vez idear un workaround?
+            variables: {
+              sign_in: {
+                button_label: 'Ingresar',
+                email_input_placeholder: 'Tu dirección de correo electrónico',
+                email_label: 'Tu dirección de correo electrónico',
+                password_input_placeholder: 'Tu contraseña',
+                password_label: 'Tu contraseña',
+                link_text: '¿Ya tienes una cuenta? Ingresa aquí',
+                loading_button_label: 'Cargando...',
+              },
+              sign_up: {
+                button_label: 'Registrarse',
+                confirmation_text: 'Revisa tu correo y sigue las instrucciones para continuar.',
+                email_input_placeholder: 'Ingresa tu correo electrónico',
+                email_label: 'Ingresa tu correo electrónico',
+                link_text: '¿No tienes cuenta? Registrate aquí',
+                loading_button_label: 'Cargando...',
+                password_input_placeholder: 'Ingresa tu contraseña',
+                password_label: 'Ingresa tu contraseña',
+              },
+              forgotten_password: {
+                button_label: 'Recuperar contraseña',
+                confirmation_text: 'Confirma tu contraseña',
+                email_input_placeholder: 'Ingresa tu correo electrónico',
+                email_label: 'Ingresa tu correo electrónico',
+                link_text: '¿Olvidaste tu contraseña?',
+                loading_button_label: 'Cargando...',
+                password_label: 'Ingresa tu contraseña',
+              },
+              magic_link: {
+                button_label: 'Enviar enlace',
+                email_input_label: 'Ingresa tu dirección de correo electrónico',
+                confirmation_text: 'Confirma que tu dirección sea correcta',
+                email_input_placeholder: 'Tu dirección de correo electrónico',
+                link_text: 'Enviar enlace de acceso único al correo',
+              },
+              update_password: {
+                button_label: 'Actualizar contraseña',
+                confirmation_text: 'Contraseña actualizada',
+                loading_button_label: 'Cargando...',
+                password_input_placeholder: 'Ingresa tu contraseña',
+                password_label: 'Ingresa tu contraseña',
+              },
+            },
+          }}
         theme="dark"
         magicLink
         providers={["github"]}

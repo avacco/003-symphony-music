e76a9ef32abf73f89fb038cd2e1c746f72862b52
Andres Vargas <a.m.vargas1996@gmail.com>
2023-06-13 17:17:01 -0300

    Añade paginas de error y cargando

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,7 +12,7 @@ export const revalidate = 0; // Previene cacheo del sitio
 export default async function Home() {
   
   const songs = await getSongs();
-  
+
   return (
     <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
       <Header>
